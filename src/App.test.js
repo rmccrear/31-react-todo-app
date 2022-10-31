51b6f1cff3435d3f27547761cbf169ba3597c038
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-31 16:22:48 -0700

    add tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
 test('renders to do list', () => {
   render(<App />);
-  const linkElement = screen.getByText(/To Do List/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/To Do List/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('should Add Todo controls', () => {
+  render(<App />);
+  expect(screen.getByTestId('item-details-input')).toBeInTheDocument();
+  expect(screen.getByTestId('assigned-to-input')).toBeInTheDocument();
+  expect(screen.getByTestId('difficulty-slider')).toBeInTheDocument();
+});
+
+test('should create a todo', async () => {
+  render(<App />);
+  const detailsInput = screen.getByTestId('item-details-input');
+  const assignInput = screen.getByTestId('assigned-to-input');
+  const submitButton = screen.getByTestId('submit-button');
+  // const slider = screen.getByTestId('difficulty-slider');
+
+  await userEvent.type(detailsInput, 'Do work');
+  await userEvent.type(assignInput, 'James');
+  await userEvent.click(submitButton);
+
+  const todoContainer = screen.getByTestId('todos-container');
+  expect(todoContainer).toHaveTextContent(/Do work/);
+  expect(todoContainer).toHaveTextContent(/James/);
+
 });
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -55,34 +55,35 @@ const Todo = () => {
 
         <label>
           <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
+          <input data-testid="item-details-input" onChange={handleChange} name="text" type="text" placeholder="Item Details" />
         </label>
 
         <label>
           <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
+          <input data-testid="assigned-to-input" onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
         </label>
 
         <label>
           <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
+          <input data-testid="difficulty-slider" onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
         </label>
 
         <label>
-          <button type="submit">Add Item</button>
+          <button data-testid="submit-button" type="submit">Add Item</button>
         </label>
       </form>
 
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
-
+      <div data-testid='todos-container'>
+        {list.map(item => (
+          <div key={item.id}>
+            <p>{item.text}</p>
+            <p><small>Assigned to: {item.assignee}</small></p>
+            <p><small>Difficulty: {item.difficulty}</small></p>
+            <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+            <hr />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
